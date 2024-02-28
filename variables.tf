00b6525b766b0b95dc6414d379aef3b2070b36e4
Omkar Vasant Nate <nate.o@northeastern.edu>
2024-02-27 20:25:54 -0500

    Add create cloudsql instance

--- variables.tf
+++ variables.tf
@@ -8,6 +8,21 @@ variable "region" {
   type        = string
 }
 
+variable "db_subnet_name" {
+  description = "The region for the resources"
+  type        = string
+}
+
+variable "private_ip_address" {
+  description = "Private IP address for the Google Compute Global Address"
+  type = object({
+    name          = string
+    purpose       = string
+    address_type  = string
+    prefix_length = number
+  })
+}
+
 variable "vpcs" {
   description = "A map of VPC configurations"
   type = map(object({
@@ -16,6 +31,10 @@ variable "vpcs" {
     routing_mode                    = string
     delete_default_routes_on_create = bool
   }))
+}
+variable "private_service_connection" {
+  description = "Google Service Networking Service Connection"
+  type        = string
 }
 
 variable "subnets" {
@@ -26,4 +45,70 @@ variable "subnets" {
     region        = string
     network       = string
   }))
+}
+
+variable "custom_images" {
+  description = "Custom images details"
+  type = object({
+    family  = string
+    project = string
+  })
+}
+
+variable "tags_for_instances" {
+  description = "Tags for instances"
+  type        = tuple([string])
+}
+
+variable "allowed_ports" {
+  description = "Allowed ports for instance"
+  type        = tuple([string])
+}
+
+variable "allowed_protocol" {
+  description = "Porotocols for firewall to allow"
+  type        = string
+}
+
+variable "firewall_rule_name" {
+  description = "Firewall name"
+  type        = string
+}
+
+variable "webapp_instance" {
+  description = "Holds VM instance information"
+  type = object({
+    machine_type              = string
+    name                      = string
+    zone                      = string
+    allow_stopping_for_update = bool
+    boot_disk_device_name     = string
+    boot_disk_auto_delete     = bool
+    boot_disk_size            = number
+    boot_disk_type            = string
+    mode                      = string
+  })
+}
+
+variable "postgres_instance" {
+  description = "Holds CloudSQL instance information for postgres"
+  type = object({
+    name                = string
+    database_version    = string
+    root_password       = string
+    deletion_protection = bool
+    tier                = string
+    availability_type   = string
+    disk_type           = string
+    disk_size           = number
+    ipv4_enabled        = bool
+  })
+}
+
+variable "webappdb" {
+  description = "PostgreSQL database information"
+  type = object({
+    name     = string
+    username = string
+  })
 }
